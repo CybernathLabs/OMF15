9bb0fba4acab00188906b4c86f7ab388cd18cbb0
Richard Stovall <rstovall@me.com>
2015-09-09 11:45:56 -0400

    testing...

--- OMF15-TargetTest2/OMF15-TargetTest2.ino
+++ OMF15-TargetTest2/OMF15-TargetTest2.ino
@@ -1,18 +1,47 @@
 #include <Servo.h>
 
+#define RESET 8
+#define TRIG 9
+
 int tStatus = 1;
 int rStatus = 1;
+
 Servo myservo;  // create servo object to control a servo
+
 void setup() {
+  Serial.begin(57600);
+  
+  pinMode(TRIG, INPUT);
+  pinMode(RESET, INPUT);
+  
   myservo.attach(9);
+
+  Serial.println("ready");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  triggerTarget();
-  delay(2000);
-  resetTarget();
-  delay(10000);
+//  triggerTarget();
+//  delay(2000);
+//  resetTarget();
+//  delay(10000);
+
+    if (digitalRead(9) == 1)
+    {
+        Serial.println("trigger");
+        triggerTarget();
+
+        delay(1000);
+
+        resetTarget();
+    }
+    if (digitalRead(8) == 1)
+    {
+        Serial.println("reset");
+        resetTarget();
+    }
+
+    delay(50);
 }
 
 void resetTarget(){
